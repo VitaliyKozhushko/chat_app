c3cc947148d1fc722a317cbd5cb91aff36833c47
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-04 16:46:28 +0300

    create models.py

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime, timezone
+
+Base = declarative_base()
+
+class User(Base):
+  __tablename__ = 'users'
+  id = Column(Integer, primary_key=True, index=True)
+  username = Column(String, unique=True, index=True, nullable=True)
+  hashed_password = Column(String, nullable=False)
+  created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+  messages = relationship('Message', back_populates='sender')
+
+class Message(Base):
+  __tablename__ = 'messages'
+
+  id = Column(Integer, primary_key=True, index=True)
+  content = Column(Text, nullable=False)
+  timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+  sender_id = Column(Integer, ForeignKey('users.id'))
+
+  sender = relationship('User', back_populates='messages')
