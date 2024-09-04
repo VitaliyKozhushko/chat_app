efc9cb4b6c3f3d74b2bb88b7fe0ec823d8cf3fad
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-04 17:10:08 +0300

    create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserBase(BaseModel):
+  username: str
+
+class UserCreate(UserBase):
+  password: str
+
+class UserResponse(UserBase):
+  id: int
+  created_at: datetime
+
+  class Config:
+    orm_mode = True
+
+class MessageBase(BaseModel):
+  content: str
+
+class MessageCreate(MessageBase):
+  pass
+
+class MessageResponse(MessageBase):
+  id: int
+  timestamp: datetime
+  sender_id: int
+
+  class Config:
+    orm_mode = True
