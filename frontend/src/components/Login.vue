e0874ed513b1074de418d2e58b202e196ec83169
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-10 18:34:52 +0300

    create default Reg

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,57 +3,66 @@
     <el-input v-model="username" placeholder="username"/>
     <el-input v-model="password" placeholder="password"/>
     <el-button type="primary" class="login-btn" @click="login">Login</el-button>
-    <el-button type="primary" @click="login" link>Sign up?</el-button>
+    <el-button type="primary" @click="registration" link>Sign up?</el-button>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 import { io } from 'socket.io-client'
+import { defineEmits } from 'vue'
 
-export default {
-  data() {
-    return {
-      username: '',
-      password: '',
-      errorMessage: '',
+const emit = defineEmits(['displayRegistration']);
+
+const registration = () => {
+  emit('displayRegistration', true);
+};
+
+const notifyParent = () => {
+  emit('updateParent', true); // Передаем значение родителю
+};
+
+const username = ref('');
+const password = ref('');
+const errorMessage = ref('');
+let socket = null;
+
+const login = async () => {
+  try {
+    const response = await fetch('http://localhost:8000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        username: username.value,
+        password: password.value,
+      }).toString(),
+    });
+
+    if (!response.ok) {
+      throw new Error('Login failed');
     }
-  },
-  methods: {
-    async login() {
-      try {
-        const response = await fetch('http://localhost:8000/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: new URLSearchParams({
-            username: this.username,
-            password: this.password,
-          }).toString(),
-        });
 
-        if (!response.ok) {
-          throw new Error('Login failed');
-        }
+    const data = await response.json();
+    const access_token = data.access_token;
 
-        const data = await response.json();
-        const access_token = data.access_token
-        this.socket = io("http://localhost:8000/", {
-          query: {
-            auth_token: access_token
-          },
-          transports: ["websocket"]
-        });
-        this.socket.on("connect", () => {
-          console.log("Connected to WebSocket server!");
-        });
-        console.log('Login successful:', data);
-      } catch (error) {
-        this.errorMessage = 'Failed to log in. Please try again.';
-        console.error('Error:', error);
-      }
-    },
-  },
+    socket = io("http://localhost:8000/", {
+      query: {
+        auth_token: access_token
+      },
+      transports: ["websocket"]
+    });
+
+    socket.on("connect", () => {
+      console.log("Connected to WebSocket server!");
+    });
+
+    console.log('Login successful:', data);
+  } catch (error) {
+    errorMessage.value = 'Failed to log in. Please try again.';
+    console.error('Error:', error);
+  }
 }
 </script>
 
--- frontend/src/views/Auth.vue
+++ frontend/src/views/Auth.vue
@@ -3,11 +3,12 @@
     <div class="logo-block">
         <img class="logo" :src="logo" alt="logo">
       </div>
-      <div v-if="!isLoginDisplay" class="sign-btns">
+      <div v-if="!isLoginDisplay && !isRegisterDisplay" class="sign-btns">
         <el-button type="primary" link @click="isLoginDisplay = !isLoginDisplay">Sign in</el-button>
-        <el-button type="primary" link>Sign up</el-button>
+        <el-button type="primary" link @click="isRegisterDisplay = !isRegisterDisplay">Sign up</el-button>
       </div>
-      <Login v-if="isLoginDisplay"/>
+      <Login v-if="isLoginDisplay" @displayRegistration="displayReg"/>
+      <Registration v-if="isRegisterDisplay"/>
   </div>
 </template>
 
@@ -16,7 +17,15 @@ import { ref } from 'vue'
 import logo from '@/assets/logo/logo.png'
 import '@/assets/scss/auth.scss'
 import Login from '@/components/Login.vue'
+import Registration from '@/components/Registration.vue'
 
 
 const isLoginDisplay = ref(false)
+const isRegisterDisplay = ref(false)
+
+const displayReg = () => {
+  isLoginDisplay.value = false
+  isRegisterDisplay.value = !isRegisterDisplay.value
+
+};
 </script>
