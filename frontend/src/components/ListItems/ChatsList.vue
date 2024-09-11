7db2b68b7047759956149bfa57f9ab83fb8c3063
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-11 08:56:45 +0300

    add cards to chats

--- frontend/src/components/ListItems/ChatsList.vue
+++ frontend/src/components/ListItems/ChatsList.vue
@@ -1,10 +1,21 @@
 <template>
-  <p>
-    ChatsList
-  </p>
+  <div class="chats-list">
+    <el-scrollbar>
+      <el-card shadow="hover">
+        <div class="user-icon">
+          <el-icon>
+            <User />
+          </el-icon>
+        </div>
+        <p>
+          Mikle
+        </p>
+      </el-card>
+    </el-scrollbar>
+  </div>
 </template>
 
 
 <script setup>
-
+import '@/assets/scss/listItems.scss'
 </script>
