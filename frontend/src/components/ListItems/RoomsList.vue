5de5aaab591da1659acb25619ae923e17379682c
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-11 09:48:04 +0300

    add 2 groups in rooms

--- frontend/src/components/ListItems/RoomsList.vue
+++ frontend/src/components/ListItems/RoomsList.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="rooms-list">
     <el-button type="primary" class="add-room" :icon="Plus">Создать</el-button>
-    <el-scrollbar>
-      <el-card shadow="hover">
-        <p>
-          Mikle
-        </p>
-      </el-card>
-    </el-scrollbar>
+    <div>
+      <el-text type="primary" size="large">Доступные комнаты</el-text>
+      <el-scrollbar>
+        <el-card shadow="hover">
+          <p>
+            Mikle
+          </p>
+        </el-card>
+      </el-scrollbar>
+    </div>
+    <div>
+      <el-text type="primary" size="large">Комнаты пользователя</el-text>
+      <el-scrollbar>
+        <el-card shadow="hover">
+          <p>
+            Mikle
+          </p>
+        </el-card>
+      </el-scrollbar>
+    </div>
   </div>
 </template>
 
