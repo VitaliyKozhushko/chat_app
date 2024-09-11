076e451551cde15862a05c2789bfd7860bf8a68a
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-11 09:06:40 +0300

    add cards to notify

--- frontend/src/components/ListItems/NotifyList.vue
+++ frontend/src/components/ListItems/NotifyList.vue
@@ -1,9 +1,19 @@
 <template>
-  <p>
-    NotifyList
-  </p>
+  <div class="notify-list">
+    <el-scrollbar>
+      <el-card shadow="hover">
+        <el-icon>
+          <Close />
+        </el-icon>
+        <p>
+          Mikle
+        </p>
+      </el-card>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
 
+import {Plus} from "@element-plus/icons-vue";
 </script>
