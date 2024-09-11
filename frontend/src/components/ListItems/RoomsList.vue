1d56a966de8eab224f900cfddf31af8d68fa1761
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-11 09:01:36 +0300

    add cards to rooms

--- frontend/src/components/ListItems/RoomsList.vue
+++ frontend/src/components/ListItems/RoomsList.vue
@@ -1,9 +1,16 @@
 <template>
-  <p>
-    RoomsList
-  </p>
+  <div class="rooms-list">
+    <el-button type="primary" class="add-room" :icon="Plus">Создать</el-button>
+    <el-scrollbar>
+      <el-card shadow="hover">
+        <p>
+          Mikle
+        </p>
+      </el-card>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
-
+import { Plus } from '@element-plus/icons-vue'
 </script>
