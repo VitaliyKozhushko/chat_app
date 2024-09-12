2af59171bab62ca39db68be0ae9a8e4a8e3e8f7d
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-12 09:47:41 +0300

    add btn to exit from private account

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -2,6 +2,7 @@
   <div class="menu">
     <div class="logo-mini">
       <img :src="logo_mini" alt="logo_mini"/>
+      <img class="exit-icon" :src="exit_icon" alt="exit" @click="handleLogout"/>
     </div>
     <div class="menu-items">
       <el-icon size="24" :class="{'active': actualItemMenu === 'chats'}" @click="() => changeMenuItem('chats')">
@@ -20,15 +21,26 @@
 <script setup>
 import '@/assets/scss/menu.scss'
 import logo_mini from '@/assets/logo/logo_small.png'
+import exit_icon from '@/assets/icons/logout.png'
 import {computed} from 'vue'
 import {useStore} from 'vuex'
+import {logout} from '@/router/auth.js'
+import { useRouter } from 'vue-router'
 
 const store = useStore()
+const router = useRouter();
 
 const actualItemMenu = computed(() => store.state.actualItemMenu)
+const socket = computed(() => store.getters.getSocket)
 
 const changeMenuItem = (newItem) => {
   store.commit('SET_ACTUAL_ITEM_MENU', newItem)
   store.commit('SET_ACTIVE_CHAT', null)
+}
+
+const handleLogout = () => {
+  logout()
+  socket.value.disconnect()
+  router.push('/')
 }
 </script>
